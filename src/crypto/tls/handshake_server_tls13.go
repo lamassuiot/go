ca77501f4349468ea067294bcc7002cbf0f6023e
Roland Shoemaker <roland@golang.org>
2026-01-26 10:49:30 -0800

    [release-branch.go1.26] Revert "crypto/tls: don't copy auto-rotated session ticket keys in Config.Clone"
    This reverts CL 736709 (commit bba24719a4cad5cc8d771fc9cfff5a38019d554a).
    
    Updates #77113
    Updates #77357
    Updates CVE-2025-68121
    
    Change-Id: I0261cb75e9adf9d0ac9890dc91ae8476b8988ba0
    Reviewed-on: https://go-review.googlesource.com/c/go/+/739320
    Reviewed-by: Coia Prant <coiaprant@gmail.com>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/740002
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Nicholas Husin <husin@google.com>
    Auto-Submit: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Nicholas Husin <nsh@golang.org>

--- src/crypto/tls/handshake_server_tls13.go
+++ src/crypto/tls/handshake_server_tls13.go
@@ -315,7 +315,6 @@ func (hs *serverHandshakeStateTLS13) checkForResumption() error {
 		return nil
 	}
 
-pskIdentityLoop:
 	for i, identity := range hs.clientHello.pskIdentities {
 		if i >= maxClientPSKIdentities {
 			break
@@ -368,13 +367,8 @@ pskIdentityLoop:
 		if sessionHasClientCerts && c.config.ClientAuth == NoClientCert {
 			continue
 		}
-		if sessionHasClientCerts {
-			now := c.config.time()
-			for _, c := range sessionState.peerCertificates {
-				if now.After(c.NotAfter) {
-					continue pskIdentityLoop
-				}
-			}
+		if sessionHasClientCerts && c.config.time().After(sessionState.peerCertificates[0].NotAfter) {
+			continue
 		}
 		opts := x509.VerifyOptions{
 			CurrentTime: c.config.time(),
