313bf9de5ea11b687fbde657ebca26b63b15d428
Michal Pristas <michal.pristas@gmail.com>
2026-02-04 09:36:59 +0000

    [release-branch.go1.26] os: support deleting inaccessible files in RemoveAll
    windows: retry file open with DELETE access after access denied
    
    Additional access rights when opening files, including SYNCHRONIZE,
    break deletion when the caller has FILE_DELETE_CHILD on the parent
    directory but not the file. Retry with DELETE only restores correct
    Windows semantics.
    
    For #77407
    
    Change-Id: Ie53bc6f1673de1a8af4dcfb7496daf99e71098cb
    GitHub-Last-Rev: 0ad635cf1a13c0242e3b1922cf47a8c594dd7215
    GitHub-Pull-Request: golang/go#77403
    Reviewed-on: https://go-review.googlesource.com/c/go/+/741040
    Reviewed-by: Quim Muntal <quimmuntal@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Auto-Submit: Michael Pratt <mpratt@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    (cherry picked from commit db5882ea9bf63849d554910f486c4c5cfea08852)
    Reviewed-on: https://go-review.googlesource.com/c/go/+/746362
    Reviewed-by: Junyang Shao <shaojunyang@google.com>
    Auto-Submit: Mark Freeman <markfreeman@google.com>

--- src/internal/syscall/windows/at_windows_test.go
+++ src/internal/syscall/windows/at_windows_test.go
@@ -131,7 +131,7 @@ func makeFileNotReadable(t *testing.T, name string) {
 			Inheritance:       windows.SUB_CONTAINERS_AND_OBJECTS_INHERIT,
 			Trustee: windows.TRUSTEE{
 				TrusteeForm: windows.TRUSTEE_IS_SID,
-				Name:        (uintptr)(unsafe.Pointer(sid)),
+				Name:        (*uint16)(unsafe.Pointer(sid)),
 			},
 		}
 	}
@@ -142,7 +142,7 @@ func makeFileNotReadable(t *testing.T, name string) {
 		entryForSid(everyoneSID),
 	}
 
-	var oldAcl, newAcl *windows.ACL
+	var oldAcl, newAcl syscall.Handle
 	if err := windows.SetEntriesInAcl(
 		uint32(len(entries)),
 		&entries[0],
@@ -160,7 +160,7 @@ func makeFileNotReadable(t *testing.T, name string) {
 		nil,
 		nil,
 		newAcl,
-		nil,
+		0,
 	); err != nil {
 		t.Fatal(err)
 	}
