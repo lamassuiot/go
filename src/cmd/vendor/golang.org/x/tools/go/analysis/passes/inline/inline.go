e23e62931f2259be7cbf48c66ee4ce285ee2bb1a
Dmitri Shuralyov <dmitshur@golang.org>
2026-03-23 14:18:47 -0400

    [release-branch.go1.26] cmd/fix: pull in CL 755980
    Fixes #78191.
    
    [git-generate]
    cd src/cmd
    go get golang.org/x/tools@internal-branch.go1.26-vendor  # v0.39.1-0.20260323181443-4f499ecaa91d
    go mod tidy
    go mod vendor
    
    Change-Id: I1162398e037c774f71421ede8c6ae8656a54eea6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/758300
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    Auto-Submit: Dmitri Shuralyov <dmitshur@google.com>

--- src/cmd/vendor/golang.org/x/tools/go/analysis/passes/inline/inline.go
+++ src/cmd/vendor/golang.org/x/tools/go/analysis/passes/inline/inline.go
@@ -324,37 +324,18 @@ func (a *analyzer) inlineAlias(tn *types.TypeName, curId inspector.Cursor) {
 	//   pkg.Id[T]
 	//   pkg.Id[K, V]
 	var expr ast.Expr = id
-	if curId.ParentEdgeKind() == edge.SelectorExpr_Sel {
+	if astutil.IsChildOf(curId, edge.SelectorExpr_Sel) {
 		curId = curId.Parent()
 		expr = curId.Node().(ast.Expr)
 	}
 	// If expr is part of an IndexExpr or IndexListExpr, we'll need that node.
 	// Given C[int], TypeOf(C) is generic but TypeOf(C[int]) is instantiated.
-	switch curId.ParentEdgeKind() {
+	switch ek, _ := curId.ParentEdge(); ek {
 	case edge.IndexExpr_X:
-		curId = curId.Parent()
-		expr = curId.Node().(*ast.IndexExpr)
+		expr = curId.Parent().Node().(*ast.IndexExpr)
 	case edge.IndexListExpr_X:
-		curId = curId.Parent()
-		expr = curId.Node().(*ast.IndexListExpr)
+		expr = curId.Parent().Node().(*ast.IndexListExpr)
 	}
-
-	// Reject inlining of a type alias used to declare an embedded
-	// struct field if doing so would change the field's name.
-	if v, ok := a.pass.TypesInfo.Defs[id].(*types.Var); ok && v.Embedded() {
-		identicalName := false
-		// TODO(adonovan): should we allow a pointer (type A = *pkg.A)?
-		if rhs, ok := alias.Rhs().(*types.Named); ok {
-			identicalName = alias.Obj().Name() == rhs.Obj().Name()
-		}
-		if !identicalName {
-			// Type is embedded, inlining the alias will cause
-			// the field name to be changed, which might break
-			// programs in terms of backwards compatibility.
-			return
-		}
-	}
-
 	t := a.pass.TypesInfo.TypeOf(expr).(*types.Alias) // type of entire identifier
 	if targs := t.TypeArgs(); targs.Len() > 0 {
 		// Instantiate the alias with the type args from this use.
